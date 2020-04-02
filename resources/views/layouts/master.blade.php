7b309955550f4df9febf7c1a2d350c5ada0aeb5a
pigslhub <59114946+pigslhub@users.noreply.github.com>
2020-04-03 02:23:05 +0500

    New Merge For Details

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,14 +2,11 @@
 <html lang="en">
   <head>
     @include('includes.partials.default.head')
-
     <title>@yield('title')</title>
     @include('includes.partials.default.styles')
   </head>
   <body>
-
     @yield('main')
-
     @include('includes.partials.default.footervarview')
     @include('includes.partials.default.scripts')
   </body>
