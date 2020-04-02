08794eebc2569d43f89a270a70b5e21d133436d9
pigslhub <59114946+pigslhub@users.noreply.github.com>
2020-04-03 01:55:23 +0500

    Comment remove

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -3,27 +3,17 @@
 <head>
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>MyFoodApp | Food Delivery Hub</title>
-    
     <link type="text/css" href="{{asset('assets/ast/css/bootstrap.min.css')}}" rel="stylesheet">
-    <!-- Fontawesome -->
     <link type="text/css" href="{{asset('assets/ast/css/font-awesome.css')}}" rel="stylesheet">
-    <!-- Flaticons -->
     <link  type="text/css"href="{{asset('assets/ast/css/font/flaticon.css')}}" rel="stylesheet">
-    <!-- Swiper Slider -->
     <link type="text/css" href="{{asset('assets/ast/css/swiper.min.css')}}" rel="stylesheet">
-    <!-- Range Slider -->
     <link type="text/css" href="{{asset('assets/ast/css/ion.rangeSlider.min.css')}}" rel="stylesheet">
-    <!-- magnific popup -->
     <link type="text/css" href="{{asset('assets/ast/css/magnific-popup.css')}}" rel="stylesheet">
-    <!-- Nice Select -->
     <link type="text/css" href="{{asset('assets/ast/css/nice-select.css')}}" rel="stylesheet">
-    <!-- Custom Stylesheet -->
     <link type="text/css" href="{{asset('assets/ast/css/style.css')}}" rel="stylesheet">
-    <!-- Custom Responsive -->
     <link type="text/css" href="{{asset('assets/ast/css/responsive.css')}}" rel="stylesheet">
-    <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700&amp;display=swap" rel="stylesheet">
-    <!-- place -->
+    
 </head>
 <body>
     <!-- Navigation -->
